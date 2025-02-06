da39115a7efb0f511b582dcb447505f0a2b6b872
Filip Bachul <filipbachul@gmail.com>
2025-02-07 00:29:04 +0100

    feat: introduce TICKER_MAP for exceptional tickets

--- src/parsers/parseTicker.test.ts
+++ src/parsers/parseTicker.test.ts
@@ -1,7 +1,7 @@
 import { equal } from "node:assert";
 import { describe, it } from "node:test";
 
-import { parseTicker } from "./parseTicker";
+import { parseTicker, TICKER_MAP } from "./parseTicker";
 
 const testCases = [
   { input: { ticker: "UPS.US" }, expected: "UPS" },
@@ -13,10 +13,20 @@ const testCases = [
 
 describe("parseQuantity", () => {
   for (const testCase of testCases) {
-    it(`should properly parse ${testCase.input.ticker}`, async () => {
+    it(`should properly parse ${testCase.input.ticker} to ${testCase.expected}`, async () => {
       const result = parseTicker(testCase.input.ticker);
 
       equal(result, testCase.expected);
     });
   }
+
+  describe("should return hardcoded value if ticker found in `TICKER_MAP`", () => {
+    for (const [ticker, expected] of Object.entries(TICKER_MAP)) {
+      it(`should properly parse ${ticker} to ${expected}`, () => {
+        const result = parseTicker(ticker);
+
+        equal(result, expected);
+      });
+    }
+  });
 });
--- src/parsers/parseTicker.ts
+++ src/parsers/parseTicker.ts
@@ -1,4 +1,12 @@
+export const TICKER_MAP: Record<string, string | undefined> = {
+  "VOW1.DE": "VOW.DE",
+} as const;
+
 export const parseTicker = (v: string) => {
+  if (TICKER_MAP[v]) {
+    return TICKER_MAP[v];
+  }
+
   const [symbol, exchange] = v.split(".");
   if (exchange === "US") {
     return symbol;
